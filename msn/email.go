dbbfd61ee8ca9dd20fcabf99177e55921a9f50b0
Soderasen AU <soderasen.au@outlook.com>
2023-10-26 09:35:59 +1100

    feat(email): support both seq and async send

--- msn/email.go
+++ msn/email.go
@@ -24,15 +24,16 @@ const (
 )
 
 type EmailServerConfig struct {
-	ServerType   EmailServerType        `json:"server_type,omitempty" yaml:"server_type,omitempty"`
-	From         *string                `json:"from,omitempty" yaml:"from,omitempty"`
-	Host         *string                `json:"host,omitempty" yaml:"host,omitempty"`
-	Port         *int                   `json:"port,omitempty" yaml:"port,omitempty"`
-	Username     *string                `json:"username,omitempty" yaml:"username,omitempty"`
-	Password     *string                `json:"password,omitempty" yaml:"password,omitempty"`
-	Encryption   *EmailServerEncryption `json:"encryption,omitempty" yaml:"encryption,omitempty"`
-	KeepAlive    *bool                  `json:"keep_alive,omitempty" yaml:"keep_alive,omitempty"`
-	TimeOutInSec *int                   `json:"timeout_in_sec,omitempty" yaml:"timeout_in_sec,omitempty"`
+	ServerType       EmailServerType        `json:"server_type,omitempty" yaml:"server_type,omitempty"`
+	From             *string                `json:"from,omitempty" yaml:"from,omitempty"`
+	Host             *string                `json:"host,omitempty" yaml:"host,omitempty"`
+	Port             *int                   `json:"port,omitempty" yaml:"port,omitempty"`
+	Username         *string                `json:"username,omitempty" yaml:"username,omitempty"`
+	Password         *string                `json:"password,omitempty" yaml:"password,omitempty"`
+	Encryption       *EmailServerEncryption `json:"encryption,omitempty" yaml:"encryption,omitempty"`
+	KeepAlive        *bool                  `json:"keep_alive,omitempty" yaml:"keep_alive,omitempty"`
+	KeepAliveTimeout *int                   `json:"keep_alive_timeout,omitempty" yaml:"keep_alive_timeout"`
+	NoopTimeout      *int                   `json:"noop_timeout,omitempty" yaml:"noop_timeout,omitempty"`
 }
 
 func (s *EmailServerConfig) Validate() *util.Result {
@@ -59,11 +60,14 @@ func (s *EmailServerConfig) Validate() *util.Result {
 }
 
 type Mailer struct {
-	client   *gomail.SMTPClient
-	inbox    chan Message
-	stopChan chan bool
-	lastSent time.Time
-	running  bool
+	server     *gomail.SMTPServer
+	client     *gomail.SMTPClient
+	inbox      chan Message
+	outbox     chan *util.Result
+	stopChan   chan bool
+	lastCreate time.Time
+	lastSent   time.Time
+	running    bool
 
 	Config EmailServerConfig
 	Logger *zerolog.Logger
@@ -85,8 +89,10 @@ func NewMailer(cfg EmailServerConfig, opts ...MailerOption) (*Mailer, *util.Resu
 	}
 
 	mailer := &Mailer{
+		server:   nil,
 		client:   nil,
 		inbox:    make(chan Message),
+		outbox:   make(chan *util.Result),
 		stopChan: make(chan bool),
 		lastSent: time.Now(),
 		running:  false,
@@ -105,11 +111,11 @@ func NewMailer(cfg EmailServerConfig, opts ...MailerOption) (*Mailer, *util.Resu
 	server.ConnectTimeout = 30 * time.Second
 	server.SendTimeout = 30 * time.Second
 	server.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-	smtpClient, err := server.Connect()
-	if err != nil {
-		return nil, util.Error("CreateSmtpClient", err)
+	mailer.server = server
+
+	if res := mailer.reconnect(); res != nil {
+		return nil, res.With("mailer.reconnect")
 	}
-	mailer.client = smtpClient
 
 	if opts != nil {
 		for _, opt := range opts {
@@ -130,11 +136,30 @@ func (m *Mailer) IsKeepAlive() bool {
 	return util.MaybeNil(m.Config.KeepAlive)
 }
 
+func (m *Mailer) reconnect() *util.Result {
+	logger := m.Logger.With().Str("mod", "mailer").Str("func", "reconnect").Logger()
+	if m.client != nil {
+		err := m.client.Close()
+		if err != nil {
+			util.LogError(&logger, "client.Close()", err)
+		}
+	}
+	var err error
+	m.client, err = m.server.Connect()
+	if err != nil {
+		return util.LogError(&logger, "server.Connect()", err)
+	}
+	m.lastCreate = time.Now()
+	logger.Info().Msg("reconnected")
+	return nil
+}
+
 func (m *Mailer) loop() {
 	logger := m.Logger.With().Str("mod", "mailer").Str("func", "loop").Logger()
 	logger.Info().Msg("start")
 	ticker := time.NewTicker(time.Duration(10) * time.Second)
-	noopTimeout := util.MaybeDefault(m.Config.TimeOutInSec, 30)
+	noopTimeout := util.MaybeDefault(m.Config.NoopTimeout, 30)
+	keepaliveTimeout := util.MaybeDefault(m.Config.KeepAliveTimeout, 300)
 	m.running = true
 	defer func() { m.running = false }()
 
@@ -148,12 +173,22 @@ func (m *Mailer) loop() {
 			return
 		case t := <-ticker.C:
 			logger.Trace().Msg("noop")
-			if t.Sub(m.lastSent).Seconds() > float64(noopTimeout) {
+			if sinceLastCreate := int(t.Sub(m.lastCreate).Seconds()); sinceLastCreate > keepaliveTimeout {
+				logger.Debug().Msgf("connection was last created at: %s, more than %d seconds. reset client", m.lastCreate.Format(time.RFC1123Z), keepaliveTimeout)
+				if res := m.reconnect(); res != nil {
+					logger.Err(res).Msg("reconnect")
+					return
+				}
+			}
+			if sinceLastSec := int(t.Sub(m.lastSent).Seconds()); sinceLastSec > noopTimeout {
 				logger.Debug().Msgf("last sent was at: %s, more than %d seconds. send noop", m.lastSent.Format(time.RFC1123Z), noopTimeout)
 				err := m.client.Noop()
 				if err != nil {
-					logger.Err(err).Msg("Noop()")
-					return
+					logger.Err(err).Msg("Noop() failed. try to reconnect to server")
+					if res := m.reconnect(); res != nil {
+						logger.Err(res).Msg("reconnect")
+						return
+					}
 				}
 				m.lastSent = time.Now()
 			}
@@ -165,10 +200,18 @@ func (m *Mailer) loop() {
 				if res != nil {
 					logger.Err(res).Msg("doSend")
 				}
+				logger.Info().Msg("email sent, getting next message ...")
 				time.Sleep(time.Duration(100) * time.Millisecond)
 			}
 		}
 	}
+}
+
+func (m *Mailer) outResult(res *util.Result) *util.Result {
+	if m.IsKeepAlive() {
+		m.outbox <- res
+	}
+	return res
 }
 
 func (m *Mailer) doSend(msg Message) *util.Result {
@@ -177,16 +220,16 @@ func (m *Mailer) doSend(msg Message) *util.Result {
 	defer func() { m.lastSent = time.Now() }()
 
 	if msg.From == "" {
-		return util.LogMsgError(&logger, "MsgTo", "there's no From")
+		return m.outResult(util.LogMsgError(&logger, "MsgTo", "there's no From"))
 	}
 	if len(msg.To) < 1 {
-		return util.LogMsgError(&logger, "MsgTo", "there's no receipts")
+		return m.outResult(util.LogMsgError(&logger, "MsgTo", "there's no receipts"))
 	}
 	if msg.Title == "" {
-		return util.LogMsgError(&logger, "MsgTo", "there's no Title")
+		return m.outResult(util.LogMsgError(&logger, "MsgTo", "there's no Title"))
 	}
 	if msg.Body == "" {
-		return util.LogMsgError(&logger, "MsgTo", "there's no Body")
+		return m.outResult(util.LogMsgError(&logger, "MsgTo", "there's no Body"))
 	}
 	email.SetFrom(msg.From).AddTo(msg.To...).SetSubject(msg.Title).SetBody(gomail.TextHTML, msg.Body)
 	logger.Info().Msgf("sending [%s] to [%v]", msg.Title, msg.To)
@@ -205,17 +248,33 @@ func (m *Mailer) doSend(msg Message) *util.Result {
 	}
 
 	if err := email.Send(m.client); err != nil {
-		return util.LogError(&logger, "SendEmail", err)
+		return m.outResult(util.LogError(&logger, "SendEmail", err))
 	}
-	return nil
+	return m.outResult(nil)
 }
 
 func (m *Mailer) Send(msg Message) *util.Result {
+	if !m.running {
+		return util.LogMsgError(m.Logger, "Send", "Mailer is not running, Panic!!")
+	}
+
 	if !m.IsKeepAlive() {
+		var err error
+		m.client, err = m.server.Connect()
+		if err != nil {
+			return util.Error("Connet", err)
+		}
+		defer m.client.Close()
 		return m.doSend(msg)
 	}
+
 	m.inbox <- msg
-	return nil
+	select {
+	case res := <-m.outbox:
+		return res
+	case <-time.After(30 * time.Second):
+		return util.LogMsgError(m.Logger, "Outbox", "timeout to get sent result")
+	}
 }
 
 func (m Mailer) Name() string {
